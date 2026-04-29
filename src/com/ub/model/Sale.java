2eb888005bb9344b2532b5cc243addff931cb90d
Shirley <yobashirley@gmail.com>
2026-04-29 10:31:03 +0100

    Update Sale.java

--- src/com/ub/model/Sale.java
+++ src/com/ub/model/Sale.java
@@ -1,6 +1,23 @@
 package model;
 
-public class Sale {
+public class Sale { 
+    private Product product;
+    private int quantitySold;
+    private double totalPrice;
 
+    public Sale(Product product, int quantitySold) {
+        this.product = product;
+        this.quantitySold = quantitySold;
+        this.totalPrice = product.getPrice() * quantitySold;
+    }
 
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void display() {
+        System.out.println(product.getName() + 
+            " | Sold: " + quantitySold + 
+            " | Total: P" + totalPrice);
+    }
 }
